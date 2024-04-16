d80dda3bfc451d7740080a576178492df89a568b
surabhiadak <surabhiadak8@gmail.com>
2024-04-16 16:40:36 +0200

    style: feature and updates related to styling

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const loadMoreBtn = document.getElementById('load-more-btn');
 let start = 0;
 const limit = 10;
   
-async function fetchRecords() {
+// Function to fetch records from the API 
+    async function fetchRecords() {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`);
     const data = await response.json();
+    loadMoreBtn.style.display="block";
     return data;
 }
 
-async function renderRecords() {
+// Function to render records on the page
+    async function renderRecords() {
     const questions = await fetchRecords();
     questions.forEach(question => {
     const faqItem = document.createElement('li');
@@ -19,6 +22,7 @@ async function renderRecords() {
         <div class="answer">${question.body}</div>
     `;
     faqItem.querySelector('.question').addEventListener ('click', () => {
+    faqItem.querySelector('.question').classList.toggle('active');
     faqItem.querySelector('.answer').classList.toggle('active');
     });
     faqList.appendChild(faqItem);
@@ -26,8 +30,10 @@ async function renderRecords() {
     start += limit;
 }
 
-    loadMoreBtn.addEventListener('click', renderRecords);
-  
+    // Function to load more records 
+    loadMoreBtn.addEventListener('click', () => {renderRecords();
+    })
+
+    // Initial rendering of records
     renderRecords();
 
-  
