f8881891c45dd3ba4884c67cc2d629e6623704aa
surabhiadak <surabhiadak8@gmail.com>
2024-05-04 08:02:30 +0200

    feat: added try-catch block for error handling

--- script.js
+++ script.js
@@ -4,32 +4,45 @@ const loadMoreBtn = document.getElementById('load-more-btn');
 let start = 0;
 const limit = 10;
   
-// Function to fetch records from the API 
+// Function to fetch records from the API   
     async function fetchRecords() {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`);
-    const data = await response.json();
-    loadMoreBtn.style.display="block";
-    return data;
-}
+        try{
+            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`);
+            const data = await response.json();
+            loadMoreBtn.style.display="block";
+            return data;
+        }
+        catch(error){
+            console.log("error fetching records", error);
+            throw error;
+        }
+    }
+    
 
 // Function to render records on the page
     async function renderRecords() {
-    const questions = await fetchRecords();
-    questions.forEach(question => {
-    const faqItem = document.createElement('li');
-    faqItem.classList.add('faq-item');
-    faqItem.innerHTML = `
-        <div class="question">${question.title}</div>
-        <div class="answer">${question.body}</div>
-    `;
-    faqItem.querySelector('.question').addEventListener ('click', () => {
-    faqItem.querySelector('.question').classList.toggle('active');
-    faqItem.querySelector('.answer').classList.toggle('active');
-    });
-    faqList.appendChild(faqItem);
-    });
-    start += limit;
-}
+        try{
+        const questions = await fetchRecords();
+        questions.forEach(question => {
+            const faqItem = document.createElement('li');
+            faqItem.classList.add('faq-item');
+            faqItem.innerHTML = `
+                <div class="question">${question.title}</div>
+                <div class="answer">${question.body}</div>
+            `;
+        faqItem.querySelector('.question').addEventListener ('click', () => {
+            faqItem.querySelector('.question').classList.toggle('active');
+            faqItem.querySelector('.answer').classList.toggle('active');
+        });
+        faqList.appendChild(faqItem);
+        });
+        start += limit;
+        }
+        catch(error){
+            console.log('error occured', error);
+        }
+    
+    }
 
     // Function to load more records 
     loadMoreBtn.addEventListener('click', () => {renderRecords();
