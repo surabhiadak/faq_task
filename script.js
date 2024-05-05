a079980784087eca8b8300b1cc453be9a4732754
Surabhi Adak <surabhiadak8@gmail.com>
2024-05-05 09:04:23 +0200

    Update: script.js

--- script.js
+++ script.js
@@ -1,55 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-const faqList = document.getElementById('faq-list');
-const loadMoreBtn = document.getElementById('load-more-btn');
-let start = 0;
-const limit = 10;
+  const faqList = document.getElementById('faq-list');
+  const loadMoreBtn = document.getElementById('load-more-btn');
+  let start = 0;
+  const limit = 10;
   
 // Function to fetch records from the API   
-    async function fetchRecords() {
-        try{
-            const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`);
-            const data = await response.json();
-            loadMoreBtn.style.display="block";
-            return data;
-        }
-        catch(error){
-            console.log("error fetching records", error);
-            throw error;
-        }
+  async function fetchRecords() {
+    try{
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${start}&_limit=${limit}`);
+      const data = await response.json();
+      loadMoreBtn.style.display="block";
+      return data;
     }
+    catch(error){
+      console.log("error fetching records", error);
+      throw error;
+    }
+  }
     
 
 // Function to render records on the page
-    async function renderRecords() {
-        try{
-        const questions = await fetchRecords();
-        questions.forEach(question => {
-            const faqItem = document.createElement('li');
-            faqItem.classList.add('faq-item');
-            faqItem.innerHTML = `
-                <div class="question">${question.title}</div>
-                <div class="answer">${question.body}</div>
-            `;
+  async function renderRecords() {
+    try{
+      const questions = await fetchRecords();
+      questions.forEach(question => {
+        const faqItem = document.createElement('li');
+        faqItem.classList.add('faq-item');
+        faqItem.innerHTML = `
+          <div class="question">${question.title}</div>
+          <div class="answer">${question.body}</div>`;
         faqItem.querySelector('.question').addEventListener ('click', () => {
-            faqItem.querySelector('.question').classList.toggle('active');
-            faqItem.querySelector('.answer').classList.toggle('active');
+          faqItem.querySelector('.question').classList.toggle('active');
+          faqItem.querySelector('.answer').classList.toggle('active');
         });
         faqList.appendChild(faqItem);
         });
-        start += limit;
-        }
-        catch(error){
-            console.log('error occured', error);
-        }
-    
+      start += limit;
     }
+    catch(error){
+      console.log('error occured', error);
+    }    
+  }
 
     // Function to load more records 
-    loadMoreBtn.addEventListener('click', () => {renderRecords();
-    })
+  loadMoreBtn.addEventListener('click', () => {
+    renderRecords();
+  })
 
     // Initial rendering of records
-    renderRecords();
+  renderRecords();
 });
 
  
